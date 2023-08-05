ec2c99cef45548396e5d64e7199d117cb33d7be0
WojciechMat <wmatsad@gmail.com>
2023-08-05 14:06:03 +0200

    better project architecture, testing some simple NNs

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import torch.optim
 import pandas as pd
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
-from model import PitchSeqNNv2
+import numpy as np
+from model import *
 from data.dataset import ComposerClassificationDataset
 
-BATCH_SIZE = 16
+BATCH_SIZE = 8
 N_EPOCHS = 50
 SEQUENCE_LENGTH = 64
 
 
-def prepare_dataset_pitch_only(dataset: ComposerClassificationDataset):
+def prepare_samples_pitch_only(dataset: ComposerClassificationDataset):
     """
-    Prepares the dataset by selecting only the pitch information from the samples and converting it
+    Prepares the samples by selecting only the pitch information from them and converting it
     into a format suitable for training.
 
     Args:
@@ -23,7 +23,7 @@ def prepare_dataset_pitch_only(dataset: ComposerClassificationDataset):
                                     and 'composer' information.
 
     Returns:
-        None: Modifies the 'samples' attribute of the dataset in-place.
+        samples (list): list of tuples (pitches, label) for ComposerClassificationDataset.samples
     """
     df = pd.DataFrame(dataset.samples)
     df = df[["notes", "composer"]]
@@ -39,31 +39,32 @@ def prepare_dataset_pitch_only(dataset: ComposerClassificationDataset):
     # pop some of the samples, to fit to batch size
     while len(samples) % BATCH_SIZE != 0:
         samples.pop(-1)
-    dataset.samples = samples
-    # print(dataset.samples[0])
+    return samples
 
 
-def train():
+def train_model(
+        model: nn.Module,
+        train_data: DataLoader,
+        lr=1e-5,
+        val_data: Optional[DataLoader] = None):
     """
-    Train the ComposerClassifier model using the ComposerClassificationDataset.
+    Train a classifier using the provided train_data. Evaluates on optional val_data
+
+     Parameters:
+        model (nn.Module): The neural network model to be trained.
+        train_data (DataLoader): The DataLoader containing the training dataset.
+        lr (float, optional): The learning rate for the Adam optimizer. Default is 1e-5.
+        val_data (DataLoader, optional): The DataLoader containing the validation dataset. Default is None.
 
     Returns:
-        PitchSeqNN: Trained model after completing the training loop.
+        nn.Module: Trained model after completing the training loop.
     """
-    # get train data
-    train_data = ComposerClassificationDataset(split="train", sequence_length=SEQUENCE_LENGTH)
-    prepare_dataset_pitch_only(train_data)
-    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
-    test_data = ComposerClassificationDataset(split="validation", sequence_length=SEQUENCE_LENGTH)
-    prepare_dataset_pitch_only(test_data)
-    # initialize model
-    model = PitchSeqNNv2()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = nn.CrossEntropyLoss()
     # training loop
     for epoch in range(N_EPOCHS):
         running_loss = 0.0
-        for i, data in enumerate(train_dataloader, 0):
+        for i, data in enumerate(train_data, 0):
             inputs, labels = data
             # comment for accumulating gradients
             optimizer.zero_grad()
@@ -74,27 +75,28 @@ def train():
 
             running_loss += loss.item()
             # Periodically print the running loss for monitoring the training progress.
-        running_loss = running_loss / len(train_dataloader)
-        val_loss = evaluate(model, test_data)
-        print(f"[{epoch + 1}] loss: {running_loss:.3f} val loss: {val_loss:.3f}")
+        running_loss = running_loss / len(train_data)
+        if val_data is not None:
+            val_loss = evaluate(model, val_data)
+            print(f"[{epoch + 1}] loss: {running_loss:.3f} val loss: {val_loss:.3f}")
+        else: print(f"[{epoch + 1}] loss: {running_loss:.3f}")
 
     print("Finished Training")
     return model
 
 
-def evaluate(model, test_data: ComposerClassificationDataset):
+def evaluate(model, test_dataloader: DataLoader):
     """
         Evaluates the performance of a given model
 
         Args:
             model (torch.nn.Module): The trained model to be evaluated.
-            test_data (ComposerClassificationDataset): The test dataset containing notes and labels.
+            test_dataloader (DataLoader): The test dataset loader containing notes and labels in batches.
 
         Returns:
             val_loss: The average loss value calculated over the test dataset.
         """
     # get test data
-    test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
     loss = 0.0
     with torch.no_grad():
         for data in test_dataloader:
@@ -105,32 +107,27 @@ def evaluate(model, test_data: ComposerClassificationDataset):
     return loss / len(test_dataloader)
 
 
-def test_model(model: Optional[nn.Module] = None, path: Optional[str] = None):
+def test_model(model: nn.Module, test_data: DataLoader, path: Optional[str] = None, classnames=None):
     """
-    Evaluate the performance of the trained ComposerClassifier model on the test dataset.
+    Evaluate the performance of the trained ComposerClassifier model with the provided test data loader.
 
     Args:
         model (PitchSeqNN): The trained ComposerClassifier model to be evaluated.
         path (str, optional): If specified, the function will load the model's state from the
                               provided path.
     """
-    # get test data
-    test_data = ComposerClassificationDataset(split="test", sequence_length=SEQUENCE_LENGTH)
-    prepare_dataset_pitch_only(test_data)
-    test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
+    if classnames is None:
+        classnames = [0, 1]
     # if path is specified load state from file
-    if model is None:
-        model = PitchSeqNNv2()
     if path is not None:
-
         model.load_state_dict(torch.load(path))
     # containers for counting prediction
-    correct_pred = {classname: 0 for classname in test_data.selected_composers}
-    total_pred = {classname: 0 for classname in test_data.selected_composers}
+    correct_pred = {classname: 0 for classname in classnames}
+    total_pred = {classname: 0 for classname in classnames}
     correct = 0
     total = 0
     with torch.no_grad():
-        for data in test_dataloader:
+        for data in test_data:
             notes, labels = data
             out = model(notes)
             loss = nn.CrossEntropyLoss()(out, labels)
@@ -141,22 +138,44 @@ def test_model(model: Optional[nn.Module] = None, path: Optional[str] = None):
 
             for label, pred in zip(labels, preds):
                 if label == pred:
-                    correct_pred[test_data.selected_composers[label]] += 1
-                total_pred[test_data.selected_composers[label]] += 1
+                    correct_pred[classnames[label]] += 1
+                total_pred[classnames[label]] += 1
     for classname, correct_count in correct_pred.items():
         accuracy = 100 * float(correct_count) / total_pred[classname]
         print(f"Accuracy for class: {classname:5s} is {accuracy:.1f} %")
-    print(f"Accuracy of the network on the test data: {100 * correct / total} %")
-    print("correctly predicted :" + str(correct_pred))
+    print(f"Accuracy of the network on the test data: {(100 * correct / total):.3f} %")
+    print(f"correctly predicted : {correct_pred}")
     return correct / total
 
 
+def get_dataloader(split="train", sequence_length=64, batch_size=BATCH_SIZE):
+    """ Create and prepare a DataLoader from ComposerClassificationDataset.
+
+    Parameters:
+        split (str): dataset split. Possible splits: "train", "test", "validation"
+        sequence_length (int): Length of a note sequence to be extracted.
+        batch_size (int): A batch size for DataLoader.
+    """
+    data = ComposerClassificationDataset(split=split, sequence_length=sequence_length)
+    data.samples = prepare_samples_pitch_only(data)
+    dataloader = DataLoader(data, batch_size=batch_size, shuffle=True)
+    return dataloader
+
+
 def main():
-    torch.manual_seed(78)
-    model = train()
-    path = "model1.pth"
+
+    models = [PitchSeqNN(), PitchSeqNNv2_64(), PitchSeqNNv3_64(), PitchSeqNNv4_64()]
+    losses = []
+    train_dataloader = get_dataloader(split="train", sequence_length=64, batch_size=16)
+    validation_dataloader = get_dataloader(split="validation", sequence_length=64, batch_size=16)
+    test_dataloader = get_dataloader(split="test", sequence_length=64, batch_size=16)
+    for model in models:
+        model = train_model(model, train_data=train_dataloader, lr=3e-4, val_data=validation_dataloader)
+        losses.append(evaluate(model, test_dataloader))
+    print(losses)
+    torch.save(models[np.argmax(losses)].state_dict(), "best.pth")
+    # path = "model1.pth"
     # torch.save(model.state_dict(), path)
-    test_model(model)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -10,30 +10,86 @@ class PitchSeqNN(nn.Module):
 
     def __init__(self):
         super(PitchSeqNN, self).__init__()
-        self.dense1 = nn.Linear(128, 256)
-        self.dense2 = nn.Linear(256, 512)
-        self.dense3 = nn.Linear(512, 256)
+        self.dense1 = nn.Linear(64, 256)
+        self.dense2 = nn.Linear(256, 256)
+        self.dense3 = nn.Linear(256, 256)
         self.dense4 = nn.Linear(256, 64)
         self.dense5 = nn.Linear(64, 2)
 
     def forward(self, x):
         x = F.relu(self.dense1(x))
-        # x = F.relu(self.dense2(x))
-        # x = F.relu(self.dense3(x))
+        x = F.relu(self.dense2(x))
+        x = F.relu(self.dense3(x))
         x = F.relu(self.dense4(x))
         x = self.dense5(x)
         return x
 
 
-class PitchSeqNNv2(nn.Module):
+class PitchSeqNNv2_64(nn.Module):
     """
     Simple sequential neural network for classifying pitch tensors of size (64, 1) into two classes.
     (performs poorly - pretty much randomly)
     """
 
     def __init__(self):
-        super(PitchSeqNNv2, self).__init__()
+        super(PitchSeqNNv2_64, self).__init__()
         self.dense1 = nn.Linear(64, 128)
+        self.dense2 = nn.Linear(128, 64)
+        self.dense3 = nn.Linear(64, 2)
+
+    def forward(self, x):
+        x = F.relu(self.dense1(x))
+        x = F.relu(self.dense2(x))
+        x = self.dense3(x)
+        return x
+
+
+class PitchSeqNNv3_64(nn.Module):
+    """
+    Simple sequential neural network for classifying pitch tensors of size (64, 1) into two classes.
+    """
+
+    def __init__(self):
+        super(PitchSeqNNv3_64, self).__init__()
+        self.dense1 = nn.Linear(64, 256)
+        self.dense2 = nn.Linear(256, 16)
+        self.dense3 = nn.Linear(16, 2)
+
+    def forward(self, x):
+        x = F.relu(self.dense1(x))
+        x = F.relu(self.dense2(x))
+        x = self.dense3(x)
+        return x
+
+
+class PitchSeqNNv4_64(nn.Module):
+    """
+    Simple sequential neural network for classifying pitch tensors of size (64, 1) into two classes.
+    """
+
+    def __init__(self):
+        super(PitchSeqNNv4_64, self).__init__()
+        self.dense1 = nn.Linear(64, 128)
+        self.dense2 = nn.Linear(128, 256)
+        self.dense3 = nn.Linear(256, 64)
+        self.dense4 = nn.Linear(64, 2)
+
+    def forward(self, x):
+        x = F.relu(self.dense1(x))
+        x = F.relu(self.dense2(x))
+        x = F.relu(self.dense3(x))
+        x = self.dense4(x)
+        return x
+
+
+class PitchSeqNNv5_32(nn.Module):
+    """
+    Simple sequential neural network for classifying pitch tensors of size (32, 1) into two classes.
+    """
+
+    def __init__(self):
+        super(PitchSeqNNv5_32, self).__init__()
+        self.dense1 = nn.Linear(32, 128)
         self.dense2 = nn.Linear(128, 64)
         self.dense3 = nn.Linear(64, 2)
 
