2a36ab997e8f06d7104e0339b730f353680f6a69
WojciechMat <wmatsad@gmail.com>
2023-08-08 14:35:28 +0200

    notes on number of samles in database

--- data/dataset.py
+++ data/dataset.py
@@ -64,7 +64,7 @@ class BagOfPitches(ComposerClassificationDataset):
         histogram = [pitch.count(it) for it in range(128)]
 
         # For seq len 64, 44 is the maximum count of a note in the train split, wtf
-        data = torch.tensor(histogram).float()
+        data = torch.tensor(histogram).float() / 64 # found out there is a sample with 64 same notes :O
         composer = self.samples[idx]["composer"]
         label = self.selected_composers.index(composer)
 
@@ -76,30 +76,53 @@ class BagOfPitches(ComposerClassificationDataset):
 
         return sample
 
+    # samples for each of chosen composers on train:
+    # composer
+    # Alexander Scriabin          294272
+    # Claude Debussy              310528
+    # Domenico Scarlatti           67712
+    # Felix Mendelssohn           358272
+    # Franz Liszt                1489536
+    # Franz Schubert             2096896
+    # Frédéric Chopin            1508096
+    # Johann Sebastian Bach       613120
+    # Johannes Brahms             419328
+    # Joseph Haydn                223232
+    # Ludwig van Beethoven       1441152
+    # Robert Schumann             897024
+    # Sergei Rachmaninoff         305280
+    # Wolfgang Amadeus Mozart     230528
+    # on validation:
+    # composer
+    # Alexander Scriabin          40960
+    # Felix Mendelssohn           22656
+    # Franz Liszt                182144
+    # Franz Schubert              17536
+    # Frédéric Chopin            179200
+    # Johann Sebastian Bach       56576
+    # Johannes Brahms            107136
+    # Joseph Haydn                29056
+    # Ludwig van Beethoven       198144
+    # Robert Schumann            138880
+    # Sergei Rachmaninoff        116608
+    # Wolfgang Amadeus Mozart     54784
+    # on test:
+    # composer
+    # Alexander Scriabin          33024
+    # Claude Debussy              68352
+    # Domenico Scarlatti          39168
+    # Felix Mendelssohn           16512
+    # Franz Liszt                230272
+    # Franz Schubert             255616
+    # Frédéric Chopin            224640
+    # Johann Sebastian Bach       62848
+    # Johannes Brahms             26624
+    # Joseph Haydn                31488
+    # Ludwig van Beethoven       228480
+    # Robert Schumann            112256
+    # Sergei Rachmaninoff         70144
+    # Wolfgang Amadeus Mozart     34688
 
-if __name__ == "__main__":
-    train_dataset = BagOfPitches(split="train")
-    count = train_dataset.df.groupby(["composer"]).size()
-    composers_with_most_data = count[count.values > 10]
-    composers_to_check = composers_with_most_data.index.tolist()
-    dataset_with_all_composers = BagOfPitches(split="train", selected_composers=composers_to_check)
-    df = pd.DataFrame()
-    maxes = []
-    i = 0
-    pbar = tqdm(dataset_with_all_composers)
-    for sample in pbar:
-        df = pd.concat([df, pd.DataFrame(sample)], ignore_index=True)
-        maxim = torch.max(sample["data"])
-        maxes.append(maxim)
-        i += 1
-        pbar.set_description(str(i))
-
-    print(np.max(maxes))
-    print(df.groupby("composer").size())
-    print(df.head())
-    # print(composers_to_check)
-    # for comp1, comp2 in itertools.combinations(composers_to_check, r=2):
-    # print([comp1, comp2])
 
     # pieces of composers:
     # Alexander Scriabin          22 # val: None
