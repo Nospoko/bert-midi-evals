11ca228ceb297a7fd80e7ded2a587cfd72b1b348
WojciechMat <wmatsad@gmail.com>
2023-08-08 13:19:01 +0200

    tests on chosen composers, analyzing database and number of samples

--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,9 @@
+# import itertools
+
 import torch
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from datasets import load_dataset
 
 
@@ -61,7 +64,7 @@ class BagOfPitches(ComposerClassificationDataset):
         histogram = [pitch.count(it) for it in range(128)]
 
         # For seq len 64, 44 is the maximum count of a note in the train split, wtf
-        data = torch.tensor(histogram).float() / 44
+        data = torch.tensor(histogram).float()
         composer = self.samples[idx]["composer"]
         label = self.selected_composers.index(composer)
 
@@ -72,3 +75,44 @@ class BagOfPitches(ComposerClassificationDataset):
         }
 
         return sample
+
+
+if __name__ == "__main__":
+    train_dataset = BagOfPitches(split="train")
+    count = train_dataset.df.groupby(["composer"]).size()
+    composers_with_most_data = count[count.values > 10]
+    composers_to_check = composers_with_most_data.index.tolist()
+    dataset_with_all_composers = BagOfPitches(split="train", selected_composers=composers_to_check)
+    df = pd.DataFrame()
+    maxes = []
+    i = 0
+    pbar = tqdm(dataset_with_all_composers)
+    for sample in pbar:
+        df = pd.concat([df, pd.DataFrame(sample)], ignore_index=True)
+        maxim = torch.max(sample["data"])
+        maxes.append(maxim)
+        i += 1
+        pbar.set_description(str(i))
+
+    print(np.max(maxes))
+    print(df.groupby("composer").size())
+    print(df.head())
+    # print(composers_to_check)
+    # for comp1, comp2 in itertools.combinations(composers_to_check, r=2):
+    # print([comp1, comp2])
+
+    # pieces of composers:
+    # Alexander Scriabin          22 # val: None
+    # Claude Debussy              37
+    # Domenico Scarlatti          18
+    # Felix Mendelssohn           24 #test: 1 val: 3
+    # Franz Liszt                 93
+    # Franz Schubert             159
+    # Frédéric Chopin            145
+    # Johann Sebastian Bach      114
+    # Johannes Brahms             20 #test: 1 val: 5
+    # Joseph Haydn                29
+    # Ludwig van Beethoven       110
+    # Robert Schumann             33
+    # Sergei Rachmaninoff         29
+    # Wolfgang Amadeus Mozart     27
--- train.py
+++ train.py
@@ -1,25 +1,20 @@
 import uuid
+import itertools
 from typing import Callable
 
 import hydra
 import torch.optim
 import torch.nn as nn
 from tqdm import tqdm
+from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 
 import wandb
-from config import Config
 from model import PitchSeqNN
 from data.dataset import BagOfPitches
 
 
-@hydra.main(config_path="conf", config_name="config")
-def main(cfg: Config):
-    dataset = BagOfPitches()
-    v_dataset = BagOfPitches(split="validation")
-    train_dataloader = DataLoader(dataset, batch_size=cfg.hyperparameters.batch_size, shuffle=True)
-    v_dataloader = DataLoader(v_dataset, batch_size=cfg.hyperparameters.batch_size)
-
+def run_one_experiment(cfg: DictConfig, train_data, val_data):
     run_id = str(uuid.uuid1())[:8]
     wandb.init(
         project="MIDI-18-bag-of-pitches",
@@ -39,14 +34,14 @@ def main(cfg: Config):
     pbar = tqdm(range(cfg.hyperparameters.num_epochs), desc="Training started!")
     for epoch in pbar:
         train_stats = training_epoch(
-            train_dataloader=train_dataloader,
+            train_dataloader=train_data,
             optimizer=optimizer,
             model=model,
             criterion=criterion,
         )
 
         v_stats = validation_epoch(
-            loader=v_dataloader,
+            loader=val_data,
             model=model,
             criterion=criterion,
         )
@@ -60,6 +55,61 @@ def main(cfg: Config):
             v_acc=v_stats["val_accuracy"],
         )
         pbar.set_description(bar)
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig):
+    dataset = BagOfPitches()
+    v_dataset = BagOfPitches(split="validation")
+    train_dataloader = DataLoader(dataset, batch_size=cfg.hyperparameters.batch_size, shuffle=True)
+    v_dataloader = DataLoader(v_dataset, batch_size=cfg.hyperparameters.batch_size)
+    count = dataset.df.groupby(["composer"]).size()
+    composers_with_most_data = count[count.values > 10]
+    composers_to_check = composers_with_most_data.index.tolist()
+    composers = [pair for pair in itertools.combinations(composers_to_check, r=2)]
+    print(f"pairs to check: {len(composers)}")
+    for pair in composers:
+        print(f"{pair[0]} vs {pair[1]}")
+        run_id = str(uuid.uuid1())[:8]
+        wandb.init(
+            project="MIDI-18-different-composers",
+            name=f"{pair[0]}-vs-{pair[1]}-{run_id}",
+            config={
+                "learning_rate": cfg.hyperparameters.lr,
+                "n_epochs": cfg.hyperparameters.num_epochs,
+                "architecture": "NN",
+                "batch_size": cfg.hyperparameters.batch_size,
+            },
+        )
+
+        model = PitchSeqNN(cfg.model.layers)
+        optimizer = torch.optim.Adam(model.parameters(), lr=cfg.hyperparameters.lr)
+
+        criterion = nn.CrossEntropyLoss()
+        pbar = tqdm(range(cfg.hyperparameters.num_epochs), desc="Training started!")
+        for epoch in pbar:
+            train_stats = training_epoch(
+                train_dataloader=train_dataloader,
+                optimizer=optimizer,
+                model=model,
+                criterion=criterion,
+            )
+
+            v_stats = validation_epoch(
+                loader=v_dataloader,
+                model=model,
+                criterion=criterion,
+            )
+
+            wandb.log({**train_stats, **v_stats})
+
+            bar = "loss={t_loss:.3f}, acc={t_acc:.2f}, val_loss={v_loss:.3f}, val_acc={v_acc:.2f}".format(
+                t_loss=train_stats["loss"],
+                t_acc=train_stats["accuracy"],
+                v_loss=v_stats["val_loss"],
+                v_acc=v_stats["val_accuracy"],
+            )
+            pbar.set_description(bar)
 
 
 def training_epoch(
